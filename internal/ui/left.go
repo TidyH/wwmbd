93c66a7fa1078a1d2772fcedc7f8ab6981b4f407
TidyH <gwaterman42293@gmail.com>
2024-11-17 21:02:42 -0600

    added method to add buttons to left side of layout

--- internal/ui/left.go
+++ internal/ui/left.go
@@ -1,45 +1,35 @@
 package ui
 
 import (
-	"fmt"
-	"wwmbd/internal/finance"
 	"wwmbd/internal/internal/config"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func newList(a *App) *widget.List {
+func newList() *widget.List {
 	data := config.StockData
+
 	list := widget.NewList(
 		func() int {
 			return len(data)
 		},
 		func() fyne.CanvasObject {
-			return container.NewPadded(
-				widget.NewLabel("          "),
-				widget.NewButton("          ", nil),
-			)
+			longestItem := data[0]
+			for i := 1; i < len(data); i++ {
+				if len(data[i]) > len(longestItem) {
+					longestItem = data[i]
+				}
+			}
+
+			return widget.NewButton(longestItem, nil) // i don't know how else to fit the text...
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*fyne.Container).Objects[0].(*widget.Label).SetText(data[int(i)])
-			o.(*fyne.Container).Objects[1].(*widget.Button).SetText(data[int(i)])
-
-			o.(*fyne.Container).Objects[1].(*widget.Button).OnTapped = func() {
-				ticker := data[int(i)]
-				tickerQuote, err := finance.GetTickerQuote(ticker)
-				if err != nil {
-					// Handle error
-				}
-
-				labelPrice := widget.NewLabel(fmt.Sprintf("Current price is: $%.2f", tickerQuote.RegularMarketPrice))
-				w4 := a.a.NewWindow(ticker)
-				w4.Resize(fyne.Size{Width: 400, Height: 200})
-				w4.SetContent(labelPrice)
-				w4.Show()
-			}
+			o.(*widget.Button).SetText(data[int(i)])
 		},
 	)
 	return list
 }
+
+// TODO: Not really sure what to use the left bar for
+//	currently just leaving some placeholder data
